b29f2e461c322c41701e8f5cd5143b7737d357a4
nvt01 <ngotung180301@gmail.com>
2021-11-16 23:37:28 +0700

    testtt

---
+++ app.js
@@ -0,0 +1,8 @@
+$('.customer-list').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    dots: true,
+    prevArrow:"<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
+    nextArrow:"<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>"
+});
